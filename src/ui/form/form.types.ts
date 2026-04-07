22e4413ceee9154b79a1549de1107f3926497882
Monica Villegas Pineda <monica.villegas@wiedii.co>
2026-04-07 11:48:42 -0500

    add component and form

---
+++ src/ui/form/form.types.ts
@@ -0,0 +1,17 @@
+export type FormValues = Record<string, string>;
+
+export interface FieldConfig {
+	name: string;
+	label: string;
+	type: string;
+	required?: boolean;
+	multiline?: boolean;
+	rows?: number;
+}
+
+export interface FormComponentProps {
+	title?: string;
+	extraFields?: FieldConfig[];
+	onSubmit: (values: Record<string, string>) => void;
+	buttonText?: string;
+}
