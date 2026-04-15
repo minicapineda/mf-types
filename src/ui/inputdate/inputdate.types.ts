55ed73d288900abb4d56a2d19614d3ca4206cc64
Monica Villegas Pineda <monica.villegas@wiedii.co>
2026-04-15 12:17:55 -0500

    update input

--- src/ui/inputdate/inputdate.types.ts
+++ src/ui/inputdate/inputdate.types.ts
@@ -1,17 +1,16 @@
 export interface DateRange {
-    start: Date | null;
-    end: Date | null;
+  start: Date | null;
+  end: Date | null;
 }
 
 export interface InputDateProps {
-    label: string;
-    value: Date | null | DateRange; 
-    onChange: (value: any) => void; 
-    placeholder?: string;
-    required?: boolean;
-    disabled?: boolean;
-    error?: boolean;
-    helperText?: string;
-    fullWidth?: boolean;
-    isRange?: boolean;
+  label: string;
+  value: Date | null | DateRange;
+  onChange: (value: Date | null | DateRange) => void;
+  required?: boolean;
+  disabled?: boolean;
+  error?: boolean;
+  helperText?: string;
+  fullWidth?: boolean;
+  isRange?: boolean;
 }
